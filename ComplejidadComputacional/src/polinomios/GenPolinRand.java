f97cee9c4fad1ba0397f37abcd48143d679b06b5
Ernesto <ernestoalbrz@outlook.com>
2016-10-06 18:25:17 -0300

    Generador actualizado
    Se generan todos los archivos en una sola pasada

--- ComplejidadComputacional/src/polinomios/GenPolinRand.java
+++ ComplejidadComputacional/src/polinomios/GenPolinRand.java
@@ -8,23 +8,25 @@ public class GenPolinRand {
 	public GenPolinRand() {
 		FileWriter fw = null;
 		PrintWriter pw = null;
-		try {
-			fw = new FileWriter("./Preparacion de Prueba/Lote de Prueba/Entrada/pruebaTiempoTodoEnUno.in");
-			pw = new PrintWriter(fw);
-				
-			Random rand = new Random();
-				
-			for (int i = 8500; i >= 0; i--) {
-				pw.println(i + " " + rand.nextInt((100 - 0) + 1));
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
+		for (int j = 50; j < 24000; j += 1500) {
 			try {
-				fw.close();
-				pw.close();
-			} catch (Exception e2) {
-				e2.printStackTrace();
+				fw = new FileWriter("./Preparacion de Prueba/Lote de Prueba/Entrada/pruebaTiempo"+j+"Elementos.in");
+				pw = new PrintWriter(fw);
+						
+				Random rand = new Random();
+					
+				for (int i = j; i >= 0; i--) {
+					pw.println(i + " " + rand.nextInt((100 - 0) + 1));
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				try {
+					fw.close();
+					pw.close();
+				} catch (Exception e2) {
+					e2.printStackTrace();
+				}
 			}
 		}
 	}
